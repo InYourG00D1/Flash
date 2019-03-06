473243966e9d0708fdb72c10a2c0244204d88200
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-03-06 20:46:50 +0700

    Add files via upload

---
+++ flash.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Welcome To Tools Prank :V 
+Jangan Dibawa Serius serius amat nanti sakit 
+*/
+
+echo "\n\e[0;31m]
+         .---.        .-----------
+        /     \  __  /    ------
+       / /     \(••)/    -----
+      //////   * \/ *   ---
+     //// / // :    : ---
+    // /   /  /*    *--
+   //          //..\\
++=============UU====UU=======================+
++              *//||\\*                       +
++               ****                         +
++ coded by : InYourG00D                      +
++ Wa : 6281382406878                         +
++ Team : LNX#CREW S.T.C BUFT                 +
++ GitHub : https://github.com/InYourG00D1    +
++============================================+\e[0m \r\n";
+echo "\n\e[0;34m]Note 62xxx jangan 08xxx"
+echo "\n\e[0;33m]Nomer: \e[0m \r\n";
+$no = trim(fgets(STDIN, 1024));
+echo "\n\e[0;33m]Jumblah Kuota Vidmax: \e[0m \r\n";
+$vid = trim(fgets(STDIN, 1024));
+echo "\n\e[0;32m]$no , Sukses Mengubah Vidmax $vid , Menjadi Kuota Flash\e[0m \r\n";
+echo "\n\e[0;34m]Thanks To \e[0m \r\n"
+?>
