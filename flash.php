cee7a9e0f6e0a066dc01420c1eb278a0e46cc590
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-03-06 20:49:22 +0700

    Update flash.php

--- flash.php
+++ flash.php
@@ -20,11 +20,11 @@ echo "\n\e[0;31m]
 + Team : LNX#CREW S.T.C BUFT                 +
 + GitHub : https://github.com/InYourG00D1    +
 +============================================+\e[0m \r\n";
-echo "\n\e[0;34m]Note 62xxx jangan 08xxx"
+echo "\n\e[0;34m]Note 62xxx jangan 08xxx";
 echo "\n\e[0;33m]Nomer: \e[0m \r\n";
 $no = trim(fgets(STDIN, 1024));
 echo "\n\e[0;33m]Jumblah Kuota Vidmax: \e[0m \r\n";
 $vid = trim(fgets(STDIN, 1024));
 echo "\n\e[0;32m]$no , Sukses Mengubah Vidmax $vid , Menjadi Kuota Flash\e[0m \r\n";
-echo "\n\e[0;34m]Thanks To \e[0m \r\n"
+echo "\n\e[0;34m]Thanks To \e[0m \r\n";
 ?>
